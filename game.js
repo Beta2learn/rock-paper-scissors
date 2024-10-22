f440afde771f5b9f06f5cec1026598ebbcb02bcb
Beta2learn <176282205+Beta2learn@users.noreply.github.com>
2024-10-22 16:09:15 +0200

    added images, modified previous js element, added eventlisteners

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
-// function playGame() {
-//     let humanScore = 0;
-//     let computerScore = 0;
-//     let roundsplayed = 0;
+    function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let roundsplayed = 0;
 
     //set document background image
     document.body.style.backgroundImage = "url(./image/background.jpg)";
@@ -72,7 +72,7 @@
     div2.appendChild(div3)
     
     // image element
-    const computerChoiceImage = document.createElement("img");
+    const computerChoiceImage = document.createElement("image");
     computerChoiceImage.style.cssText = `
     width: 100px;
     height: 100px;
@@ -89,6 +89,160 @@
     font-weight: bold;`;
     container.appendChild(messageDisplay);
 
+    // function to show message
+    function showMessage(message){
+        messageDisplay.textContent = message;
+    }
+
+    // function to get computer choice
+    function getComputerChoice(){
+         const ComputerChoice = Math.floor(Math.random() * 3);
+            if (ComputerChoice === 0) {
+                    return "rock";
+            } else if (ComputerChoice === 1) {
+                    return "paper";
+            } else {
+                    return "scissors";
+                    }
+                }
+    
+    //function to create image choice
+
+    function createChoice(imgSrc, altText, labelText, choice) {
+        const choiceContainer = document.createElement("div");
+        choiceContainer.style.cssText = `
+        display: inline-block;
+        text-align: center;
+        margin: 10px;
+        border-radius: 8px;`;
+
+
+        const image = document.createElement("image");
+        img.src = imgSrc;
+        img.alt = altText;
+        img.width = 100;
+        img.height = 100;
+        img.style.borderRadius = "8px";
+
+        const label = document.createElement("span");
+        label.textContent = labelText;
+        label.style.cssText = `
+        display: block;
+        font-size: 15px;`;
+        
+        // eventlistener for choice/image selection
+        image.addEventListener("click", function () {
+            if (roundsPlayed < 5) { 
+            const computerChoice = getComputerChoice();
+            playRound(choice, computerChoice);
+            const allImages = document.querySelectorAll("#humanselection image");
+                allImages.forEach(image => {
+                    image.style.border = "none";
+                });
+            }
+            img.style.border = "2px dotted red";
+        });
+
+        //append image and label to choice container
+        choiceContainer.appendChild(img);
+        choiceContainer.appendChild(label);
+
+        return choiceContainer;
+    }
+
+    // create & append game choices
+    div.appendChild(createChoice("./image/rock.png", "rock", "Rock", "rock"));
+    div.appendChild(createChoice("./image/paper.png", "paper", "Paper", "paper"));
+    div.appendChild(createChoice("./image/scissors.png", "scissors", "Scissors", "scissors"));
+
+    //element to display score
+    const humanScoreDisplay = document.createElement("p");
+    humanScoreDisplay.textContent = "Human Score: 0";
+    humanScoreDisplay.style.fontWeight = "bold";
+    container.appendChild(humanScoreDisplay);
+
+    const computerScoreDisplay = document.createElement("p");
+    computerScoreDisplay.textContent = "Computer Score: 0";
+    computerScoreDisplay.style.fontWeight = "bold";
+    container.appendChild(computerScoreDisplay);
+
+    //reset game
+    const resetButton = document.createElement("button");
+    resetButton.textContent = "Reset Game";
+    resetButton.style.cssText = `
+    margin: 50px;
+    padding: 10px 15px;
+    font-weight: bold;
+    border-radius: 5px;
+    border: none;
+    background-color: #f44336;
+    color: white;
+    cursor: pointer;`;
+    container.appendChild(resetButton);
+    resetButton.addEventListener("click", function(){
+        humanScore = 0;
+        computerScore = 0;
+        roundsPlayed = 0;
+        humanScoreDisplay.textContent = "Human Score: 0";
+        computerScoreDisplay.textContent = "Computer Score: 0";
+        showMessage("Game reset! Make a selection to begin");
+        computerChoiceImg.src = " ";
+        computer
+        
+    });
+
+    // playfunction
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+
+        if (computerChoice === "rock") {
+            computerChoiceImg.src = "./image/rock.png";
+        } else if (computerChoice === "paper") {
+            computerChoiceImg.src = "./image/paper.png";
+        } else {
+            computerChoiceImg.src = "./image/scissors.png";
+        }
+
+        if (humanChoice === computerChoice) {
+            showMessage("It's a tie!");
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "rock")
+        ) {
+            showMessage(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        } else {
+            showMessage(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+
+        //update score display
+        humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
+        computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+
+        // increment rounds played
+        roundsPlayed++;
+
+        //play 5 rounds
+        if (roundsPlayed === 5) {
+            choicesEnabled = false;
+
+            if (humanScore > computerScore) {
+                showMessage("Congratulations! You win!");
+            } else if (computerScore > humanScore) {
+                showMessage("Computer wins!");
+            } else {
+                showMessage("Well done, it's a tie!");
+            }
+        }
+
+        console.log(`Score: Human ${humanScore} - Computer ${computerScore}`);
+    }
+}
+
+// Run the game
+playGame();
 
 
 
