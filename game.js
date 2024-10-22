da9ef65903ffe71de706da402770e1014ede8c24
Beta2learn <176282205+Beta2learn@users.noreply.github.com>
2024-10-22 14:47:16 +0200

    added 1st div for header elements and modified html file

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let roundsplayed = 0;
+// function playGame() {
+//     let humanScore = 0;
+//     let computerScore = 0;
+//     let roundsplayed = 0;
 
     //set document background image
-    document.body.style.backgroundImage = "url(./img/background.jpg)";
+    document.body.style.backgroundImage = "url(./image/background.jpg)";
     document.body.style.backgroundSize = "cover";
     document.body.style.backgroundRepeat = "no repeat";
 
@@ -23,65 +23,68 @@ function playGame() {
     align-items: center;
     align-content: center;
     padding: 10px;
-    overflow-y: auto; 
-    `;
+    overflow-y: auto; `;
     document.body.appendChild(container);
-    
-    function getComputerChoice() {
-        const ComputerChoice = Math.floor(Math.random() * 3);
 
-        if (ComputerChoice === 0) {
-                    return "rock";
-                } else if (ComputerChoice === 1) {
-                    return "paper";
-                } else {
-                    return "scissors";
-                }
-            }
+    //created div for  header elements
+    const header = document.createElement("div")
+    container.appendChild(header);
+    
+    // function getComputerChoice() {
+    //     const ComputerChoice = Math.floor(Math.random() * 3);
+
+    //     if (ComputerChoice === 0) {
+    //                 return "rock";
+    //             } else if (ComputerChoice === 1) {
+    //                 return "paper";
+    //             } else {
+    //                 return "scissors";
+    //             }
+    //         }
     
     
-    function getHumanChoice(){
-        let input = prompt("Enter rock, paper or scissors").toLowerCase(); 
-    while (input === null || (input !== "rock" && input !== "paper" & input !== "scissors")){
-        input = prompt("Invalid input. Please enter rock, paper, or scissors").toLowerCase();
-    }
-    return input;
-    }
+    // function getHumanChoice(){
+    //     let input = prompt("Enter rock, paper or scissors").toLowerCase(); 
+    // while (input === null || (input !== "rock" && input !== "paper" & input !== "scissors")){
+    //     input = prompt("Invalid input. Please enter rock, paper, or scissors").toLowerCase();
+    // }
+    // return input;
+    // }
     
-    function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
+    // function playRound(humanChoice, computerChoice){
+    // humanChoice = humanChoice.toLowerCase();
     
-    if(humanChoice === computerChoice) {
-        console.log("It's a tie!");
-    } else if(
-        (humanChoice === "rock" && computerChoice === "scissors") ||
-        (humanChoice === "scissors" && computerChoice === "paper") ||
-        (humanChoice === "paper" && computerChoice === "rock")
-        ){
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-            humanScore++;           } else {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
-        console.log(`score: Human ${humanScore} - computer ${computerScore}`);
-        }
+    // if(humanChoice === computerChoice) {
+    //     console.log("It's a tie!");
+    // } else if(
+    //     (humanChoice === "rock" && computerChoice === "scissors") ||
+    //     (humanChoice === "scissors" && computerChoice === "paper") ||
+    //     (humanChoice === "paper" && computerChoice === "rock")
+    //     ){
+    //         console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    //         humanScore++;           } else {
+    //         console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    //         computerScore++;
+    //     }
+    //     console.log(`score: Human ${humanScore} - computer ${computerScore}`);
+    //     }
         
-            };
+    //         };
         
 
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+    //     const humanSelection = getHumanChoice();
+    //     const computerSelection = getComputerChoice();
     
-    playRound(humanSelection, computerSelection);
+    // playRound(humanSelection, computerSelection);
         
     
-    if(humanScore > computerScore) {
-        console.log("Congratulations! You win!")
-        } else if( computerScore > humanScore){
-            console.log("Computer wins!")
-        } else{
-        console.log("Weldone, it's a tie!")
-        }
+    // if(humanScore > computerScore) {
+    //     console.log("Congratulations! You win!")
+    //     } else if( computerScore > humanScore){
+    //         console.log("Computer wins!")
+    //     } else{
+    //     console.log("Weldone, it's a tie!")
+    //     }
     
     
-    playGame();
+    // playGame();
